bd72e91fd7353d6c5fd3c7239a63bb93cc713770
Chu <82406635+chuyun07@users.noreply.github.com>
2026-04-22 10:39:38 -0400

    Update Food.java

--- GreenFoot/Food.java
+++ GreenFoot/Food.java
@@ -1,18 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Food here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;
 public class Food extends Actor
 {
     private int gravity = 5;
-    /**
-     * Act - do whatever the Food wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public Food()
     {
         double randNum = Math.random();
